3799251b78d4e15fa6f20e126368689966836614
a-rudak-EIS <arudak@ebsco.com>
2025-04-04 16:42:51 +0500

    feat: deploy server

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,54 @@
-import * as cdk from 'aws-cdk-lib';
+import { CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Cors, LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class ApiGatewayStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const appFunction = new NodejsFunction(this, 'appFunction', {
+      runtime: Runtime.NODEJS_22_X,
+      entry: '../src/main.ts',
+      handler: 'handler',
+      memorySize: 128,
+      timeout: Duration.seconds(30),
+      bundling: {
+        externalModules: [
+          'aws-sdk',
+          'class-validator',
+          'class-transformer',
+          '@nestjs/microservices',
+          '@nestjs/websockets',
+        ],
+        forceDockerBundling: false,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new RestApi(this, 'appApi', {
+      restApiName: 'App Service',
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+        ],
+      },
+    });
+
+    const rootResource = api.root;
+    rootResource.addProxy({
+      defaultIntegration: new LambdaIntegration(appFunction),
+      anyMethod: true,
+    });
+
+    new CfnOutput(this, 'ApiGatewayUrl', {
+      value: api.url,
+      description: 'API Gateway Endpoint for App Service',
+    });
   }
 }
